200e851df542aef4e4f0e862e676fb8f7a27e48a
Purshotam1 <singhpurshotam48@gmail.com>
2020-07-12 10:00:17 +0530

    Reversing Dependencies 	Design Decisions: 1) Initialiazing Gear Class Variable inside Wheel Class 			  2) Calling Gear Class to get gearInches by passing diameter 	Implementaion Decisions: Defining new method getGear to create new Gear Class Object during constructor call of Wheel Class

--- GearsDemo.java
+++ GearsDemo.java
@@ -2,44 +2,16 @@ package assignment;
 
 import java.util.Objects;
 
-interface RoundObject {
-	public double getDiameter(); 
-}
-
-class Wheel implements RoundObject {
-	final private double rim, tire;
-
-	public Wheel(double rim, double tire) {
-		this.rim = rim;
-		this.tire = tire;
-	}
-
-	public double getDiameter() {
-		return rim + (tire * 2);
-	}
-
-	public double getRim() {
-		return rim;
-	}
-
-	public double getTire() {
-		return tire;
-	}
-}
-
 class Gear {
 	final private double chainring, cog;
-	private RoundObject wheel = null;
+
+	public Gear() {
+		this(0, 0);
+	}
 
 	public Gear(double chainring, double cog) {
 		this.chainring = chainring;
 		this.cog = cog;
-	}
-
-	public Gear(double chainring, double cog, RoundObject wheel) {
-		this.chainring = chainring;
-		this.cog = cog;
-		this.wheel = wheel;
 	}
 
 	public double getChainring() {
@@ -48,10 +20,6 @@ class Gear {
 
 	public double getCog() {
 		return cog;
-	}
-
-	public RoundObject getWheel() {
-		return wheel;
 	}
 
 	public double getRatio() {
@@ -63,24 +31,71 @@ class Gear {
 		}
 	}
 
-	public double getGearInches() {
-		if (wheel != null) {
-			return getRatio() * wheel.getDiameter();
-		} else {
-			System.out.println("Wheel variable is null");
-			return -1;
-		}
+	public double getGearInches(double diameter) {
+		return getRatio() * diameter;
 	}
 }
 
+
+class Wheel {
+	final private double rim, tire;
+	final private Gear gear;
+
+	public Wheel() {
+		this(0, 0);
+	}
+
+	public Wheel(double rim, double tire) {
+		this.rim = rim;
+		this.tire = tire;
+		this.gear = null;
+	}
+
+	public Wheel(double rim, double tire, double chainring, double cog) {
+		this.rim = rim;
+		this.tire = tire;
+		this.gear = getGear(chainring, cog);
+	}
+
+	public double getRim() {
+		return rim;
+	}
+
+	public double getTire() {
+		return tire;
+	}
+
+	private Gear getGear(double chainring, double cog) {
+		return new Gear(chainring, cog);
+	}
+
+	public double getDiameter() {
+		return rim + (tire * 2);
+	}
+
+	public double getGearInches() {
+		if (gear != null) {
+			return gear.getGearInches(getDiameter());
+		} else {
+			System.out.println("Gear is null");
+			return -1;
+		}
+	} 
+
+}
+
+
 public class GearsDemo {
 	public static void playWithGears() {
-		Wheel wheel1 = new Wheel(26, 1.5);
-		Gear gear1 = new Gear(52, 11);
-		System.out.println(gear1.getRatio());
 
-		Gear gear2 = new Gear(52, 11, wheel1);
-		System.out.println(gear2.getGearInches());
+		Gear gear = new Gear(52, 11);
+		System.out.println(gear.getRatio());
+
+		Wheel wheel1 = new Wheel(26, 1.5);
+		System.out.println(wheel1.getDiameter());
+
+		Wheel wheel2 = new Wheel(26, 1.5, gear.getChainring(), gear.getCog());
+		System.out.println(wheel2.getGearInches());
 	}
 
 	public static void main(String[] args) {
